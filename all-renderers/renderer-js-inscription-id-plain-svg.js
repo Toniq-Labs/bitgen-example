e52d61cd523429d1f75ad8107b33f118e8e3c51f
electrovir <electrovir@users.noreply.github.com>
2023-06-22 01:31:40 +0000

    Deploying to gh-pages from @ Toniq-Labs/bitgen-example@481c6d79b52fc4b6aac6556fe14879de89f8e204 🚀

--- all-renderers/renderer-js-inscription-id-plain-svg.js
+++ all-renderers/renderer-js-inscription-id-plain-svg.js
@@ -4,14 +4,35 @@
  */
 
 async function render(size, ...inscriptionIds) {
-    const innerImages = inscriptionIds.map(
-        (id) =>
-            `<image width="${size.width}px" height="${size.height}px" href="/bitgen-example/content/${id}" />`,
-    );
+    const innerImages = inscriptionIds.map((id) => {
+        return /* HTML */ `
+            <foreignObject x="0" y="0" width="100%" height="100%">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 ${size.width} ${size.height}"
+                    width="${size.width}"
+                    height="${size.height}"
+                    style="image-rendering: pixelated; background: url(/bitgen-example/content/${id}) no-repeat center/100%;"
+                ></svg>
+            </foreignObject>
+        `;
+    });
 
-    const flattenedSvgCode = `<svg xmlns="http://www.w3.org/2000/svg" width="${
-        size.width
-    }" height="${size.height}">${innerImages.join('')}</svg>`;
+    const flattenedSvgCode = /* HTML */ `
+        <svg xmlns="http://www.w3.org/2000/svg" width="${size.width}" height="${size.height}">
+            ${innerImages.join('')}
+        </svg>
+    `;
 
-    return `<style>body, html {margin: 0;padding: 0;overflow: hidden;}</style>${flattenedSvgCode}`;
+    return /* HTML */ `
+        <style>
+            body,
+            html {
+                margin: 0;
+                padding: 0;
+                overflow: hidden;
+            }
+        </style>
+        ${flattenedSvgCode}
+    `;
 }
