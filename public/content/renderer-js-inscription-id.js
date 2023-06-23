c9a6bcfdad380247f1f2cc3ae1b9915241f5ffc6
electrovir <electrovir@users.noreply.github.com>
2023-06-23 01:14:57 +0000

    revert back to nested SVGs cause the other way caused more problems
    also add compact svg blob renderer

--- public/content/renderer-js-inscription-id.js
+++ public/content/renderer-js-inscription-id.js
@@ -4,87 +4,72 @@
  */
 
 async function render(size, ...inscriptionIds) {
-    /**
-     * All images must be converted to base64 so that they can be self contained within the final
-     * combined image. This is needed because SVGs loaded via the "src" attribute in an <img>
-     * element (which is how the final combined image is loaded) do not load nested images from over
-     * the network.
-     */
-    const base64Images = await Promise.all(
-        inscriptionIds.map(async (id) => {
-            const image = await fetch(`/bitgen-example/content/${id}`);
-            const base64 = await getBase64(await image.blob());
+    try {
+        /**
+         * All images must be converted to base64 so that they can be self contained within the
+         * final combined image. This is needed because SVGs loaded via the "src" attribute in an
+         * <img> element (which is how the final combined image is loaded) do not load nested images
+         * from over the network.
+         */
+        const base64Images = await Promise.all(
+            inscriptionIds.map(async (id) => {
+                const image = await fetch(`/bitgen-example/content/${id}`);
+                return await getBase64(await image.blob());
+            }),
+        );
+        /**
+         * Inner SVGs must be generated so that animated GIFs will render correctly as a standalone
+         * tab in all browsers. Using background urls on an SVG allows "image-rendering: pixelated"
+         * to work in all browsers.
+         */
+        const innerImages = base64Images.map((base64Image) => {
+            return /* HTML */ `
+                <foreignObject x="0" y="0" width="100%" height="100%">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 ${size.width} ${size.height}"
+                        width="${size.width}"
+                        height="${size.height}"
+                        style="image-rendering: pixelated; background: url(${base64Image}) no-repeat center/100%;"
+                    ></svg>
+                </foreignObject>
+            `;
+        });
 
-            return base64;
-        }),
-    );
-
-    const finalImageUrl = generatedCombinedImageUrl(size, base64Images, false);
-
-    /**
-     * This fixes a bug in some browsers where resized images with transparent backgrounds have
-     * transparency edge render artifacts, like grey borders. As long as this image gets 1 pixel
-     * rendered on screen for 1 frame, the artifacts will go away.
-     */
-    const resizeArtifactFix = `
-        <img
-            class="full-size"
-            onload="this.remove()"
-            src="${generatedCombinedImageUrl(size, base64Images, true)}"
-        />
-    `;
-
-    return /* HTML */ `
-        <style>
-            /*
-                Remove default browser padding/margin for when this is loaded in an iframe.
-            */
-            body,
-            html {
-                margin: 0;
-                padding: 0;
-                overflow: hidden;
-            }
-            .full-size {
-                position: absolute;
-                opacity: 1;
-                top: calc(100% - 1px);
-                left: calc(100% - 1px);
-            }
-        </style>
-        ${resizeArtifactFix}
-        <img src="${finalImageUrl}" />
-    `;
-}
-
-function generatedCombinedImageUrl(size, base64Images, fullSize) {
-    const innerImages = base64Images.reverse().map((base64Image) => {
-        return `url(${base64Image}) no-repeat ${fullSize ? '' : 'center/contain'}`;
-    });
-
-    const flattenedSvgCode = /* HTML */ `
-        <svg
-            ${fullSize ? 'class="full-size"' : ''}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 ${size.width} ${size.height}"
-            width="${size.width}"
-            height="${size.height}"
-            style="image-rendering: pixelated; background: ${innerImages.join(', ')};"
-        ></svg>
-    `;
-    const flattenedSvgBlob = new Blob([flattenedSvgCode], {type: 'image/svg+xml'});
-    return URL.createObjectURL(flattenedSvgBlob);
+        const flattenedSvgCode = /* HTML */ `
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 ${size.width} ${size.height}"
+                width="${size.width}"
+                height="${size.height}"
+            >
+                ${innerImages.join('')}
+            </svg>
+        `;
+        const flattenedSvgBlobUrl = URL.createObjectURL(
+            new Blob([flattenedSvgCode], {type: 'image/svg+xml'}),
+        );
+        return /* HTML */ `
+            <style>
+                body,
+                html {
+                    margin: 0;
+                    padding: 0;
+                    overflow: hidden;
+                }
+            </style>
+            <img src="${flattenedSvgBlobUrl}" />
+        `;
+    } catch (error) {
+        return `<p style="color: red;">${error?.message || String(error)}</p>`;
+    }
 }
 
 function getBase64(file) {
     return new Promise((resolve, reject) => {
         const reader = new FileReader();
-        reader.onload = () => {
-            resolve(reader.result);
-        };
-        reader.onerror = (error) => {
-            reject(error);
-        };
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = (error) => reject(error);
         reader.readAsDataURL(file);
     });
 }
